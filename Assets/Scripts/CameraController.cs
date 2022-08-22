49890f02e0d0c95d9cd1923ed47f2cdaa60bb4d0
sukruErc <sukrucan.ercoban@outlook.com>
2022-08-22 11:31:32 +0300

    Count Master

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
